016dbaef639606bff40a7177c21b7159b199731e
Azamat Nurov <azamat@mancho.dev>
2023-04-06 15:00:54 +0600

    scss ==> css

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,13 @@ import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
-import {} from "react-router";
+import { BrowserRouter } from "react-router-dom";
 
 // Local dependencies
 import { theme } from "./Components/Layout/Theme";
 import { setUpStore } from "./Redux/Store/Store";
 import App from "./App";
-import "./index.scss";
-import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
