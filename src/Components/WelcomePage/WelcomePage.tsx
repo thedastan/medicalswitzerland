c75598a82be5fe167e2eae09f106e13042df2de9
Azamat Nurov <azamat@mancho.dev>
2023-04-10 13:41:02 +0600

    added croppper in medical

---
+++ src/Components/WelcomePage/WelcomePage.tsx
@@ -0,0 +1,55 @@
+/* External dependencies */
+import { Box, Text } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+
+/* Local dependencies */
+import "./style.scss";
+
+export default function WelcomePage() {
+  const [isVisible, setIsVisible] = useState(true);
+  const [display, setDisplay] = useState("block");
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDisplay("none");
+    }, 2100);
+    return () => clearTimeout(timer);
+  });
+
+  return (
+    <Box
+      bg="black"
+      pos="fixed"
+      left="0"
+      right="0"
+      top="0"
+      bottom="0"
+      zIndex="3"
+      display={display}
+      className={`welcome-container ${isVisible ? "visible" : ""}`}
+    >
+      <Box minH="100%" display="flex" flexDir="column" justifyContent="center">
+        <Text color="#C7C4C4" textAlign="center" fontSize="22px" mb="24px">
+          medical
+          <span style={{ color: "#E11F26" }}>switzerland</span>
+        </Text>
+        <Text
+          textColor="#C7C4C4"
+          textAlign="center"
+          fontSize="32px"
+          fontFamily="inter"
+          fontWeight="700"
+        >
+          PATIENTENAKTE
+        </Text>
+      </Box>
+    </Box>
+  );
+}
