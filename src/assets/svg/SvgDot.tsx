e736277691de00a088824db08d2e61dd49e13011
thedastan <dastanshermatov@gmail.com>
2023-05-17 18:40:44 +0600

    Change texts, fontsizes, border remove of dots, welcome texts bottom

--- src/assets/svg/SvgDot.tsx
+++ src/assets/svg/SvgDot.tsx
@@ -1,13 +1,13 @@
 export default function SvgDot() {
   return (
     <svg
-      width="9"
-      height="9"
+      width="11"
+      height="11"
       viewBox="0 0 9 9"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <circle cx="4.5" cy="4.5" r="4" fill="white" stroke="black" />
+      <circle cx="4.5" cy="4.5" r="4" fill="white"/>
     </svg>
   );
 }
