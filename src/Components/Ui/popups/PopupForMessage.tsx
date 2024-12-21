7f273cb0aaf76cabb172a099e162d9f04b18b979
thedastan <dastanshermatov@gmail.com>
2024-12-22 02:56:02 +0600

    animation & text

--- src/Components/Ui/popups/PopupForMessage.tsx
+++ src/Components/Ui/popups/PopupForMessage.tsx
@@ -5,8 +5,8 @@ import SvgSuccess from "../../../assets/svg/SvgSuccess";
 import { useAppSelector } from "../../../Hooks/Hooks";
 import { useActionsForMessage } from "../../../Hooks/useActions";
 import SvgKey from "../../../assets/svg/SvgKey";
-import "./style.scss";
 import { Trans } from "react-i18next";
+import "./style.scss";
 
 export default function PopupForMessage() {
   const {
@@ -26,20 +26,139 @@ export default function PopupForMessage() {
   } = useAppSelector((state) => state.messageReducer);
 
   useEffect(() => {
-    setTimeout(() => {
-      ActionUpload(false);
-      ActionSuccess(false);
-    }, 3000);
+    if (success || upload) {
+      const timer = setTimeout(() => {
+        ActionUpload(false);
+        ActionSuccess(false);
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
   }, [success, upload]);
 
   useEffect(() => {
-    setTimeout(() => {
-      ActionError(false);
-      ActionReset(false);
-      ActionUpload(false);
-      ActionErrorMessenger("");
-    }, 4500);
+    if (error || reset_password) {
+      const timer = setTimeout(() => {
+        ActionError(false);
+        ActionReset(false);
+        ActionUpload(false);
+        ActionErrorMessenger("");
+      }, 4500);
+      return () => clearTimeout(timer);
+    }
   }, [upload, reset_password, error]);
+
+  const renderContent = () => {
+    if (success) {
+      return (
+        <Box
+          display="flex"
+          flexDir="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Text
+            mb="15px"
+            color="white"
+            fontSize="15px"
+            fontFamily="inter"
+            textAlign="center"
+          >
+            <Trans>{successMessenger.title}</Trans>
+          </Text>
+          <Box mb="9px">
+            <SvgSuccess />
+          </Box>
+          <Text
+            color="white"
+            fontSize="10px"
+            fontWeight="300"
+            fontFamily="inter"
+          >
+            <Trans>{successMessenger.desc}</Trans>
+          </Text>
+        </Box>
+      );
+    }
+
+    if (reset_password) {
+      return (
+        <Box
+          display="flex"
+          flexDir="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Text
+            mb="16px"
+            color="white"
+            fontSize="15px"
+            fontWeight="500"
+            fontFamily="inter"
+          >
+            <Trans>resetYourPasword</Trans>
+          </Text>
+          <SvgKey />
+          <Text
+            mt="10px"
+            color="white"
+            fontSize="12px"
+            fontWeight="200"
+            fontFamily="inter"
+            textAlign="center"
+          >
+            <Trans>descReset</Trans>
+          </Text>
+        </Box>
+      );
+    }
+
+    if (upload) {
+      return (
+        <Box
+          display="flex"
+          flexDir="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Text
+            mb="15px"
+            color="white"
+            fontSize="15px"
+            fontWeight="400"
+            fontFamily="inter"
+            textAlign="center"
+          >
+            <Trans>done</Trans>
+          </Text>
+          <Box>
+            <SvgSuccess />
+          </Box>
+        </Box>
+      );
+    }
+
+    if (error) {
+      return (
+        <Box
+          display="flex"
+          flexDir="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Text
+            color="white"
+            fontSize="15px"
+            fontWeight="400"
+            textAlign="center"
+            fontFamily="inter"
+          >
+            <Trans>{errorMessenger}</Trans>
+          </Text>
+        </Box>
+      );
+    }
+    return null;
+  };
 
   return (
     <>
@@ -52,139 +171,32 @@ export default function PopupForMessage() {
         }`}
       />
       <Box
+        w={{ base: "100%", md: "600px" }}
         left="0"
         right="0"
         h="100vh"
-        pt="20px"
-        px="10px"
         zIndex="6"
         pos="fixed"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
         className={`popupMessage ${
           success || error || reset_password || upload ? "transform" : ""
         }`}
       >
         <Box
+          w={{ base: "100%", md: "500px" }}
           px="8px"
           h="151px"
           rounded="12px"
           bg="thirdlittleGray"
           boxShadow="0px 4px 4px rgba(20, 20, 20, 0.25), 0px 4px 4px #000000"
+          display="flex"
+          flexDir="column"
+          alignItems="center"
+          justifyContent="center"
         >
-          <Box
-            h="151px"
-            display="flex"
-            flexDir="column"
-            className="popup"
-            alignItems="center"
-            justifyContent="center"
-          >
-            {success && (
-              <Box
-                display="flex"
-                flexDir="column"
-                alignItems="center"
-                justifyContent="center"
-              >
-                <Text
-                  mb="15px"
-                  color="white"
-                  fontSize="15px"
-                  fontFamily="inter"
-                  textAlign="center"
-                >
-                  <Trans>{successMessenger.title}</Trans>
-                </Text>
-                <Box mb="9px">
-                  <SvgSuccess />
-                </Box>
-                <Text
-                  color="white"
-                  fontSize="10px"
-                  fontWeight="300"
-                  fontFamily="inter"
-                >
-                  <Trans>{successMessenger.desc}</Trans>
-                </Text>
-              </Box>
-            )}
-            {reset_password && (
-              <Box
-                display="flex"
-                flexDir="column"
-                alignItems="center"
-                justifyContent="center"
-              >
-                <Text
-                  mb="16px"
-                  color="white"
-                  fontSize="15px"
-                  fontWeight="500"
-                  fontFamily="inter"
-                >
-                  <Trans>resetYourPasword</Trans>
-                </Text>
-                <SvgKey />
-                <Text
-                  mt="10px"
-                  color="white"
-                  fontSize="12px"
-                  fontWeight="200"
-                  fontFamily="inter"
-                  textAlign="center"
-                >
-                  <Trans>descReset</Trans>
-                </Text>
-                {/* <Text
-                  color="white"
-                  fontSize="10px"
-                  fontWeight="200"
-                  fontFamily="inter"
-                >
-                  Please check inbox or spam folder
-                </Text> */}
-              </Box>
-            )}
-            {upload && (
-              <Box
-                display="flex"
-                flexDir="column"
-                alignItems="center"
-                justifyContent="center"
-              >
-                <Text
-                  mb="15px"
-                  color="white"
-                  fontSize="15px"
-                  fontWeight="400"
-                  fontFamily="inter"
-                  textAlign="center"
-                >
-                  <Trans>uploadTitle</Trans>
-                </Text>
-                <Box>
-                  <SvgSuccess />
-                </Box>
-              </Box>
-            )}
-            {error && (
-              <Box
-                display="flex"
-                flexDir="column"
-                alignItems="center"
-                justifyContent="center"
-              >
-                <Text
-                  color="white"
-                  fontSize="15px"
-                  fontWeight="400"
-                  textAlign="center"
-                  fontFamily="inter"
-                >
-                  <Trans>{errorMessenger}</Trans>
-                </Text>
-              </Box>
-            )}
-          </Box>
+          {renderContent()}
         </Box>
       </Box>
     </>
